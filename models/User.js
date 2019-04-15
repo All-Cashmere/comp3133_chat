c361d007a454a32458485d3ee00071f9a7923007
All-Cashmere <dakyung934@gmail.com>
2019-04-15 17:57:43 -0400

    Timestamp added on the schema

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
     socketId:  { type: String, required: true },
     nickname: String,
+    timestamp: { type : Date, default: Date.now }
 });
 
 module.exports = mongoose.model('user', userSchema);
